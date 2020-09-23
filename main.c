fc6c1c202b19c3c100ee733c901cf8f28e6dcef5
Guilherme Girão <guigirao@hotmail.com>
2020-09-23 10:17:03 -0300

    :art: Adiciona nova estutura ao projeto

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main () {
-  printf("Teste");
+  printf("olá, mundo!");
 
   return 0;
 }
--- manu.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main(){
-  printf("manu");
-  return(0);
-}
