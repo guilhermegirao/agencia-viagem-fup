f126d51127ddf8771bedf9e8ed77e8138744b394
Guilherme Girão <guigirao@hotmail.com>
2020-09-29 09:55:46 -0300

    Revert "Update vendasFuncoes.c"
    This reverts commit 20ef51fc8ae5da8fa57898dccadfd3cb17a0dbea.

--- vendas/vendasFuncoes.c
+++ vendas/vendasFuncoes.c
@@ -1,203 +1,225 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <string.h>
 
-#define FILE_VENDAS "./db/vendas.txt"
+#define FILE_PACOTES "pacotes.txt"
 
-typedef struct vendas {
-  int id, cpf_cliente, id_pacote;
-  struct Data {
-    int dia, mes, ano;
-  } data;
-} Vendas;
+typedef struct pacote
+{
+    long int idPacote;
+    int numHospedes;
+    char nomePacote[100], destinoViagem[100], dataPartida[10], dataRetorno[10];
+}Pacote;
 
-int vendasExiste () {
-  FILE *f;
-  int existe = 0;
+void cadastrarPacotes(){
+    FILE *fab = fopen(FILE_PACOTES, "ab+");
 
-  if ((f = fopen(FILE_VENDAS, "r+"))) {
-    fclose(f);
-    existe = 1;
-  }
+    Pacote p;
+    
+    printf("\n=============================\n");
+    printf("Exemplo de modelo:");
+    printf("\n=============================\n");
+    printf("ID: 12345 (apenas numeros)\n");
+    printf("Nome do pacote: Pacote Villa Hotel\n");
+    printf("Destino: Recife-PE\n");
+    printf("Numero de hospedes: 8 (apenas numeros)\n");
+    printf("Data de partida: 12/03\n");
+    printf("Data de retorno: 16/04");
+    printf("\n=============================\n");
 
-  return existe;
+    printf("ID do pacote: ");
+    scanf("%ld", &p.idPacote);
+
+    printf("Nome do pacote: ");
+    scanf(" %[^\n]", p.nomePacote);
+
+    printf("Destino: ");
+    scanf(" %[^\n]", p.destinoViagem);
+
+    printf("Numero de hospedes: ");
+    scanf("%d", &p.numHospedes);
+
+    printf("Data de partida: ");
+    scanf(" %[^\n]", p.dataPartida);
+
+    printf("Data de retorno: ");
+    scanf(" %[^\n]", p.dataRetorno);
+
+    fwrite(&p, sizeof(Pacote), 1, fab);
+
+    fclose(fab);
 }
 
-void adicionarVenda () {
-  FILE *f = fopen(FILE_VENDAS, "ab+");
+void listarPacotes(){
 
-  time_t timeVenda;
-  Vendas v;
+    //checa se o arquivo possui ou nao pacotes
 
-  int id = 0;
+    FILE *frb = fopen(FILE_PACOTES, "rb");
+    Pacote p;
+    int cont_elementos = 0;
 
-  printf("Cadastro de Venda:");
-  printf("\n=============================");
-  printf("\nInsira o CPF do Cliente: ");
-  scanf("%d", &v.cpf_cliente);
-  printf("Insira o ID do Pacote de Viagem requirido: ");
-  scanf("%d", &v.id_pacote);
+    while(fread(&p, sizeof(Pacote), 1, frb)){ //cont_elementos: numero de pacotes
+        cont_elementos++;
+    }
+    fclose(frb);
+    
+    // se cont_elementos = 0, nao ha pacotes no arquivo
 
-  timeVenda = time(NULL);
-  struct tm tm = *localtime(&timeVenda);
+    if(cont_elementos == 0){
 
-  v.data.dia = tm.tm_mday;
-  v.data.mes = tm.tm_mon + 1;
-  v.data.ano = tm.tm_year + 1900;
+        printf("\nNao ha pacotes cadastrados.\n");
 
-  //Gera ID da venda
-  if (vendasExiste() == 1) {
-    Vendas v2;
-    FILE *frb = fopen(FILE_VENDAS, "rb");
+    }else{
+        FILE *frb2 = fopen(FILE_PACOTES, "rb");
 
-    while (fread(&v2, sizeof(Vendas), 1, frb)) id = v2.id;
-    id++;
+        while(fread(&p, sizeof(Pacote), 1, frb)){
+        printf("\n=============================\n");
+        printf("ID do pacote: %ld\n", p.idPacote);
+        printf("Nome do pacote: %s\n", p.nomePacote);
+        printf("Destino: %s\n", p.destinoViagem);
+        printf("Numero de hospedes: %d\n", p.numHospedes);
+        printf("Data de partida: %s\n", p.dataPartida);
+        printf("Data de retorno: %s", p.dataRetorno);
+        printf("\n=============================\n");
+        }
+
+    }
 
     fclose(frb);
-  }
-
-  v.id = id;
-
-  fwrite(&v, sizeof(Vendas), 1, f);
-
-  fclose(f);
-
-  printf("\nCadastro realizado com sucesso.");
 }
 
-void listarVendas () {
-  FILE *f = fopen (FILE_VENDAS, "rb");
-  Vendas v;
+void consultarPacotes(){
 
-  if (vendasExiste() == 1) {
-    printf("Vendas realizadas: ");
-    printf("\n=============================\n");
+    //checa se o arquivo possui ou nao pacotes
 
-    while (fread(&v, sizeof(Vendas), 1, f)) {
-      printf("ID da Venda: %d\n", v.id);
-      printf("CPF do Cliente: %d\n", v.cpf_cliente);
-      printf("ID do Pacote: %d\n", v.id_pacote);
-      printf("Data da venda: %02d/%02d/%d\n",  v.data.dia, v.data.mes, v.data.ano);
-      printf("=============================\n");
+    FILE *frb = fopen(FILE_PACOTES, "rb");
+    Pacote p;
+    int cont_elementos = 0;
+
+    while(fread(&p, sizeof(Pacote), 1, frb)){
+        cont_elementos++;
     }
-  } else {
-    printf("=============================\n");
-    printf("Nenhuma venda foi realizada.");
-    printf("\n=============================\n");
-  }
+    fclose(frb);
 
-  fclose(f);
+    FILE *frb2 = fopen(FILE_PACOTES, "rb");
+    int op, cont = 0;
+
+    // se cont_elementos = 0, nao ha pacotes no arquivo
+
+    if(cont_elementos == 0){
+
+        printf("\nNao ha pacotes cadastrados.\n");
+
+    }else{
+
+        printf("\nDigite o ID do pacote que deseja consultar: ");
+        scanf("%d", &op);
+
+        while(fread(&p, sizeof(Pacote), 1, frb2)){
+            if(p.idPacote == op){
+                printf("\n=============================\n");
+                printf("ID do pacote: %ld\n", p.idPacote);
+                printf("Nome do pacote: %s\n", p.nomePacote);
+                printf("Destino: %s\n", p.destinoViagem);
+                printf("Numero de hospedes: %d\n", p.numHospedes);
+                printf("Data de partida: %s\n", p.dataPartida);
+                printf("Data de retorno: %s", p.dataRetorno);
+                printf("\n=============================\n");
+                cont++;
+            }
+        }
+        fclose(frb2);
+
+        if(cont == 0){
+            printf("\nNao ha pacotes com o ID informado.\n");
+        }
+    }
 }
 
-void listarVendasCliente () {
-  int clienteCpf, contagem = 0;
-  FILE *f = fopen (FILE_VENDAS, "rb");
-  Vendas v;
+void removerPacotes(){
 
-  if (vendasExiste() == 1) {
-    printf("Insira o CPF do Cliente para realizar a pesquisa: ");
-    scanf("%d", &clienteCpf);
+    //checa se o arquivo possui ou nao pacotes
+    
+    FILE *frb = fopen(FILE_PACOTES, "rb");
+    Pacote p;
+    int cont_elementos = 0;
 
-    printf("\nVendas realizadas para o Cliente de CPF %d: ", clienteCpf);
-    printf("\n=============================\n");
-
-    while (fread(&v, sizeof(Vendas), 1, f)) {
-      if (v.cpf_cliente == clienteCpf) {
-        printf("ID da Venda: %d\n", v.id);
-        printf("ID do Pacote: %d\n", v.id_pacote);
-        printf("Data da venda: %02d/%02d/%d\n",  v.data.dia, v.data.mes, v.data.ano);
-        printf("=============================\n");
-
-        contagem++;
-      }
+    while(fread(&p, sizeof(Pacote), 1, frb)){
+        cont_elementos++;
     }
+    fclose(frb);
 
-    if (contagem == 0) {
-      printf("Não há vendas realizadas para o CPF do Cliente indicado.\n");
-      printf("=============================\n");
+    // se cont_elementos = 0, nao ha pacotes no arquivo
+    
+    if(cont_elementos == 0){
+
+        printf("\nNao ha pacotes cadastrados.\n");
+
+    }else{
+
+        FILE *frb2 = fopen(FILE_PACOTES, "rb");
+        Pacote *pac = (Pacote*) malloc(sizeof(Pacote));
+        int id, cont = 0;
+
+        printf("Digite o ID do pacote a ser removido: ");
+        scanf("%ld", &id);
+
+        while(fread(&p, sizeof(Pacote), 1, frb)){
+            if(p.idPacote != id){
+                *(pac + cont) = p;
+                cont++;
+                pac = (Pacote*) realloc(pac, (cont + 1) * sizeof(Pacote));
+            }
+        }
+        fclose(frb2);
+
+        FILE *fwb = fopen(FILE_PACOTES, "wb");
+
+        for(int i = 0; i < cont; i++){
+            fwrite(pac+i, sizeof(Pacote), 1, fwb);
+        }
+        fclose(fwb);
     }
-  } else {
-    printf("=============================\n");
-    printf("Nenhuma venda foi realizada.");
-    printf("\n=============================\n");
-  }
-
-  fclose(f);
 }
 
-void listarVendasPacote () {
-  int pacoteId, contagem = 0;
-  FILE *f = fopen (FILE_VENDAS, "rb");
-  Vendas v;
+void menuPacotes(){
 
-  if (vendasExiste() == 1) {
-    printf("Insira o ID do Pacote para realizar a pesquisa: ");
-    scanf("%d", &pacoteId);
+    int op;
 
-    printf("\nVendas realizadas para o Pacote de ID %d: ", pacoteId);
-    printf("\n=============================\n");
+    do{
+        printf("\n=============================\n");
+        printf("Menu de pacotes:");
+        printf("\n=============================\n");
+        printf("1. Cadastrar um pacote\n");
+        printf("2. Listar pacotes\n");
+        printf("3. Consultar pacotes\n");
+        printf("4. Remover um pacote\n");
+        printf("5. Encerrar programa");
+        printf("\n=============================\n");
+        printf("Digite aqui sua opcao: ");
+        scanf("%d", &op);
 
-    while (fread(&v, sizeof(Vendas), 1, f)) {
-      if (v.id_pacote == pacoteId) {
-        printf("ID da Venda: %d\n", v.id);
-        printf("CPF do Cliente: %d\n", v.cpf_cliente);
-        printf("Data da venda: %02d/%02d/%d\n",  v.data.dia, v.data.mes, v.data.ano);
-        printf("=============================\n");
+        switch(op){
+            case 1:
+                cadastrarPacotes();
+                break;
+            case 2:
+                listarPacotes();
+                break;
+            case 3:
+                consultarPacotes();
+                break;
+            case 4:
+                removerPacotes();
+                break;
+            case 5:
+                printf("Encerrando o programa...");
+                break;
+            default:
+                printf("\nOpcao invalida, tente outra.\n");
+        }
 
-        contagem++;
-      }
-    }
+    }while(op != 5);
 
-    if (contagem == 0) {
-      printf("Não há vendas realizadas para o ID do Pacote indicado.\n");
-      printf("=============================\n");
-    }
-  } else {
-    printf("=============================\n");
-    printf("Nenhuma venda foi realizada.");
-    printf("\n=============================\n");
-  }
-
-  fclose(f);
-}
-
-void removerVenda () {
-  int vendaId, contagem = 0;
-  FILE *f = fopen (FILE_VENDAS, "rb");
-  Vendas v;
-
-  if (vendasExiste() == 1) {
-    Vendas *vd = (Vendas*) malloc(sizeof(Vendas));
-
-    printf("Insira o ID da Venda a ser removida: ");
-    scanf("%d", &vendaId);
-
-    while (fread(&v, sizeof(Vendas), 1, f)) {
-      if (v.id != vendaId) {
-        *(vd + contagem) = v;
-        contagem++;
-
-        vd = (Vendas*) realloc(vd, (contagem + 1) * sizeof(Vendas));
-      }
-    }
-
-    fclose(f);
-
-    if (contagem != 0) {
-      FILE *fw = fopen (FILE_VENDAS, "wb");
-
-      for (int i = 0; i < contagem; i++) {
-        fwrite((vd + i), sizeof(Vendas), 1, fw);
-      }
-
-      fclose(fw);
-    } else {
-      printf("Não há resultados para a venda indicada.\n");
-      printf("=============================\n");
-    }
-  } else {
-    printf("=============================\n");
-    printf("Nenhuma venda foi realizada.");
-    printf("\n=============================\n");
-  }
+    return (0);
 }
